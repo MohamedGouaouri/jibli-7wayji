e066f2269b799dd22accbb31d34511b2a9a2187a
MohamedGouaouri <im_gouaouri@esi.dz>
2021-12-23 08:51:33 +0100

    login and registration pages finished

--- resources/js/typeit.js
+++ resources/js/typeit.js
@@ -1,6 +1,12 @@
-new TypeIt("#typeit", {
-    strings: ["This is a great string.", "But here is a better one."],
+new TypeIt("#services", {
+    strings: ["All about our services"],
     speed: 50,
-    loop: true,
+    cursor: false,
     waitUntilVisible: true,
+}).go();
+
+new TypeIt("#stats h2", {
+    speed: 50,
+    waitUntilVisible: true,
+    cursor: false
 }).go();
